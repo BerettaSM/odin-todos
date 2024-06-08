b0661667018531e9891948af8fd6d005a1a2ffa3
Ramon Saviato <ramonsaviato@hotmail.com>
2024-06-08 18:39:15 -0300

    refactor: change ValidationError constructor to take in errors

--- src/app/errors/index.ts
+++ src/app/errors/index.ts
@@ -1,5 +1,9 @@
 export class ApplicationError extends Error {}
 
-export class ValidationError extends ApplicationError {}
+export class ValidationError extends ApplicationError {
+  constructor(public errors: Record<string, string[]>) {
+    super('Validation error');
+  }
+}
 
 export class ObjectNotFoundError extends ApplicationError {}
