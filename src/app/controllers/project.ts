3fd22fcaf397e45addc2ae0c6d55f7b9873c92c4
Ramon Saviato <ramonsaviato@hotmail.com>
2024-06-07 23:15:37 -0300

    add logic to render projects/todos on controller

--- src/app/controllers/project.ts
+++ src/app/controllers/project.ts
@@ -1,10 +1,15 @@
-import { type SubmittedProject } from '../domain';
-import { ProjectService } from '../services';
+import {
+  type SubmittedTodo,
+  type SubmittedProject,
+  type Project,
+} from '../domain';
+import { ProjectService, TodoService } from '../services';
 import { createProjectElement } from '../../dom';
 
 export class ProjectController {
   constructor(
-    private service: ProjectService,
+    private projectService: ProjectService,
+    private todoService: TodoService,
     private controlledNode: HTMLElement,
   ) {}
 
@@ -15,17 +20,34 @@ export class ProjectController {
     // TODO: Filter out projects that have no todo,
     //       no need to render them.
 
-    const allProjects = this.service.findAll();
+    const allProjects = this.projectService.findAll();
 
     for (const project of allProjects) {
-      const ele = createProjectElement(project);
-      this.controlledNode.appendChild(ele);
+      this.renderProject(project);
     }
   }
 
   addNewProject(project: SubmittedProject) {
-    const newProject = this.service.save(project);
-    const ele = createProjectElement(newProject);
-    this.controlledNode.appendChild(ele);
+    const newProject = this.projectService.save(project);
+    this.renderProject(newProject);
+  }
+
+  addNewTodo(todo: SubmittedTodo) {
+    const newTodo = this.todoService.save(todo);
+    const project = this.projectService.findById(newTodo.projectId);
+    this.renderProject(project);
+  }
+
+  renderProject(project: Project) {
+    const ele = createProjectElement(project);
+    const existingProject = this.controlledNode.querySelector(
+      `[data-project-id="${project.id}"]`,
+    );
+
+    if (existingProject) {
+      this.controlledNode.replaceChild(ele, existingProject);
+    } else {
+      this.controlledNode.appendChild(ele);
+    }
   }
 }
