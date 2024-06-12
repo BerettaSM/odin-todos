21a47a9c57deac8f64deabedbf1ddb38d7de7a04
Ramon Saviato <ramonsaviato@hotmail.com>
2024-06-12 01:00:28 -0300

    feat: add new create todo button on project header

--- src/dom/project.ts
+++ src/dom/project.ts
@@ -1,4 +1,5 @@
 import { type Project } from '../app/domain';
+import { ElementConfig } from '../types';
 
 import { classifyTodos } from '../utils';
 import { createElement } from './helper';
@@ -6,6 +7,42 @@ import { createEmptyTodoSectionElement, createTodoElement } from './todo';
 
 export function createProjectElement(project: Project) {
   const todoSections = classifyTodos(project.todos);
+
+  const actionButtonsDescriptions = [
+    {
+      tag: 'button',
+      properties: {
+        class: 'icon-button',
+        'aria-label': 'Add project',
+        'data-action': 'add-todo',
+        'data-project-id': project.id,
+      },
+      children: [
+        {
+          tag: 'i',
+          properties: {
+            class: 'fa-solid fa-plus fa-lg icon',
+          },
+        },
+      ],
+    },
+    {
+      tag: 'button',
+      properties: {
+        class: 'icon-button',
+        'aria-label': 'Delete project',
+        'data-project-action': 'delete-project',
+      },
+      children: [
+        {
+          tag: 'i',
+          properties: {
+            class: 'fa-solid fa-trash fa-lg icon close-icon',
+          },
+        },
+      ],
+    },
+  ].slice(0, project.id !== '@DEFAULT_PROJECT' ? 2 : 1) as ElementConfig[];
 
   return createElement({
     tag: 'article',
@@ -32,27 +69,7 @@ export function createProjectElement(project: Project) {
             properties: {
               class: 'project-actions',
             },
-            children:
-              project.id !== '@DEFAULT_PROJECT'
-                ? [
-                    {
-                      tag: 'button',
-                      properties: {
-                        class: 'icon-button',
-                        'aria-label': 'Delete project',
-                        'data-project-action': 'delete-project',
-                      },
-                      children: [
-                        {
-                          tag: 'i',
-                          properties: {
-                            class: 'fa-solid fa-trash fa-lg icon close-icon',
-                          },
-                        },
-                      ],
-                    },
-                  ]
-                : undefined,
+            children: actionButtonsDescriptions,
           },
         ],
       },
